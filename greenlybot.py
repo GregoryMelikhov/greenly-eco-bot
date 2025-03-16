4dcb48295e4af8fe8caa343efa2fb792eb51bfe9
Gregory Melikhov <melikhovgregory@gmail.com>
2025-03-16 13:43:10 +0200

    Add files via upload

---
+++ greenlybot.py
@@ -0,0 +1,23 @@
+import telebot
+from greenlylogic import craft_xmas_ball, sorting, time_decomposition_of, about
+
+token = "8053418554:AAEsArDlcqPvnf2oxMXeySX4HZiVEPNe6Ic"
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, about)
+
+@bot.message_handler(commands=['crafts'])
+def send_crafts(message):
+    bot.reply_to(message, craft_xmas_ball)
+
+@bot.message_handler(commands=['sorting'])
+def send_sorting_way(message):
+    bot.reply_to(message, sorting)
+print("Bot working!")
+
+@bot.message_handler(commands=['decompositionof'])
+def send_time_decomposition_of(message):
+    bot.reply_to(message, time_decomposition_of)
+bot.polling()
