57b069a0798ec430fbe4f9517ca369431cfffbe2
Gregory Melikhov <melikhovgregory@gmail.com>
2025-03-16 13:44:38 +0200

    Update greenlybot.py

--- greenlybot.py
+++ greenlybot.py
@@ -1,7 +1,7 @@
 import telebot
 from greenlylogic import craft_xmas_ball, sorting, time_decomposition_of, about
 
-token = "8053418554:AAEsArDlcqPvnf2oxMXeySX4HZiVEPNe6Ic"
+token = ""
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
